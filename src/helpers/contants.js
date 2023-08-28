11643f88d645ab1787ac7b4c737e4281d257fa69
MONE'M <60390230+Ahmed1monm@users.noreply.github.com>
2023-08-28 21:24:53 +0300

    change backend url

--- src/helpers/contants.js
+++ src/helpers/contants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost/php-rest-api/index.php/products"
+// export const BASE_URL = "http://localhost/php-rest-api/index.php/products"
+export const BASE_URL = "http://68.183.73.169/scandiweb-server/index.php/products/"
 
 export const units = {
     weight: 'KG',
